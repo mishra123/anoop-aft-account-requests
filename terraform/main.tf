26f81b1c5deee6b3d591b547cefe71165e6dff81
anoop.mishra <anoop.mishra@mantalus.com>
2026-06-04 17:33:50 +0930

    Use native AFT account request resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,10 +13,8 @@ locals {
   }
 }
 
-module "account_requests" {
+resource "aft_account_request" "accounts" {
   for_each = local.account_requests
-
-  source = "git::https://github.com/aws-ia/terraform-aws-control_tower_account_factory.git//modules/aft-account-request?ref=1.20.0"
 
   control_tower_parameters = {
     AccountEmail              = each.value.email
